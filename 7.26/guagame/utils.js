839844acc61a1f03bbe15e92fb5d1aeba456f15e
Xiao Haiping <xiaohp1993@gmail.com>
2017-08-11 08:19:31 +0800

    add plane game

---
+++ 7.26/guagame/utils.js
@@ -0,0 +1,24 @@
+const e = sel => document.querySelector(sel)
+
+const log = console.log.bind(console)
+
+const imageFromPath = function(path) {
+    var img = new Image()
+    img.src = path
+    return img
+}
+
+const rectIntersects = function(a, b) {
+    var o = a
+    if (b.y > o.y && b.y < o.y + o.image.height) {
+        if (b.x > o.x && b.x < o.x + o.image.width) {
+            return true
+        }
+    }
+    return false
+}
+
+const randomBetween = function(start, end) {
+    var n = Math.random() * (end - start + 1)
+    return Math.floor(n + start)
+}
